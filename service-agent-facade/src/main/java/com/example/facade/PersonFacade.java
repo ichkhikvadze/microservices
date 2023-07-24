b043686b03c64f75c0fce2eed4b879d5a051dd61
Ioseb Chkhikvadze <iosebi12344@gmail.com>
2023-07-25 00:23:26 +0400

    Add import

--- service-agent-facade/src/main/java/com/example/facade/PersonFacade.java
+++ service-agent-facade/src/main/java/com/example/facade/PersonFacade.java
@@ -1,5 +1,6 @@
 package com.example.facade;
 
+import com.example.domain.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
